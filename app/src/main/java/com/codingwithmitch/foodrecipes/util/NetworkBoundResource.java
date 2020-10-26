7b90d65817a6c3fa3e39ab1f1b548c8bf53c5797
Seungwon <jshtmddnjs@naver.com>
2020-10-26 20:29:13 +0900

    NetworkBoundResource logic-1

--- app/src/main/java/com/codingwithmitch/foodrecipes/util/NetworkBoundResource.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/NetworkBoundResource.java
@@ -2,20 +2,64 @@ package com.codingwithmitch.foodrecipes.util;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+import com.codingwithmitch.foodrecipes.AppExecutors;
 import com.codingwithmitch.foodrecipes.requests.responses.ApiResponse;
 
 // CacheObject: Type for the Resource data. (database cache)
 // RequestObject: Type for the API response. (network request)
-
-// this kind of abstract class cannot be instantiated by itself, but can be instantiated by being extended from other class.
 public abstract class NetworkBoundResource<CacheObject, RequestObject> {
 
+    private static final String TAG = "NetworkBoundResource";
+
+    private AppExecutors appExecutors;
     private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+
+    public NetworkBoundResource(AppExecutors appExecutors) {
+        this.appExecutors = appExecutors;
+        init();
+    }
+
+    private void init(){
+
+        // update LiveData for loading status
+        results.setValue((Resource<CacheObject>) Resource.loading(null));
+
+        // observe LiveData source from local db
+        final LiveData<CacheObject> dbSource = loadFromDb();
+
+        results.addSource(dbSource, new Observer<CacheObject>() {
+            @Override
+            public void onChanged(@Nullable CacheObject cacheObject) {
+
+                results.removeSource(dbSource);
+
+                if(shouldFetch(cacheObject)){
+                    // get data from the network
+
+                }
+                else{
+                    results.addSource(dbSource, new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(@Nullable CacheObject cacheObject) {
+                            setValue(Resource.success(cacheObject));
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void setValue(Resource<CacheObject> newValue){
+        if(results.getValue() != newValue){
+            results.setValue(newValue);
+        }
+    }
 
     // Called to save the result of the API response into the database.
     @WorkerThread
@@ -40,5 +84,3 @@ public abstract class NetworkBoundResource<CacheObject, RequestObject> {
         return results;
     };
 }
-
-

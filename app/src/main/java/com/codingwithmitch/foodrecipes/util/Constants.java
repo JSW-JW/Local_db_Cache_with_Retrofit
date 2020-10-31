58da82526ecbc1116ab3d54cdcc9d560e9332eab
Seungwon <jshtmddnjs@naver.com>
2020-10-31 23:15:57 +0900

    cache refresh time constraint

--- app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/util/Constants.java
@@ -10,6 +10,8 @@ public class Constants {
     public static final int READ_TIMEOUT = 2; // 2 seconds
     public static final int WRITE_TIMEOUT = 2; // 2 seconds
 
+    public static final long RECIPE_REFRESH_TIME = 60 * 60 * 24 * 30; // 30 days to refresh recipe
+
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
             {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
 
